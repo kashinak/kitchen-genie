8f22bffe9dbb01cf23fb38443f4023f84a2b8ced
kashinak <kashinakfx@gmail.com>
2021-09-25 00:39:52 +0000

    Create Flask templates, inherit from base.html so not to duplicate code in addition create routing to link nav links to corresponding pages or templates

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import os, render_template
+import os
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,6 +8,56 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/group")
+def group_recipes():
+    return render_template("group_recipes.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/goals")
+def goals():
+    return render_template("goals.html")
+
+
+@app.route("/recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
+@app.route("/recipes")
+def your_recipes():
+    return render_template("your_recipes.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
 
 
 if __name__ == "__main__":
