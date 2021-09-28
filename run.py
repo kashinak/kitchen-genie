a18c646eb8af2e5cc39445774b32cfb533d7f1e1
kashinak <kashinakfx@gmail.com>
2021-09-28 19:43:44 +0000

    Add secrect_key variable to run.py to allow for flash functionality

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -19,7 +22,8 @@ def about():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form["email"])
+        flash("Thank you! {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
