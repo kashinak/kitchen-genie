16c8fa3add08bc212d02ed0cdce8a23fac26535a
kashinak <kashinakfx@gmail.com>
2021-09-30 20:30:35 +0000

    Add configurations to run.py: MONGO_DBNAME to grab database name, MONGO_URI to config connection string, SECRET_KEY using Flask functions also import additonal Flask functions: redirect, session, request and url_for

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
+from flask import (
+    Flask, render_template, request, 
+    flash, redirect, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -8,12 +10,23 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("group_recipes.html", recipes=recipes)
 
 
 @app.route("/about")
