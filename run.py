8f3331d8edf8cd22136e32f3834e27899d025367
kashinak <kashinakfx@gmail.com>
2021-10-13 01:21:56 +0000

    Add created_by, dynamic ingredients and preparation inputs to add_recipe.html page

--- run.py
+++ run.py
@@ -55,10 +55,15 @@ def add_recipe():
     prep_times = mongo.db.prep_times.find().sort("prep_time", 1)
     cook_times = mongo.db.cook_times.find().sort("cook_time", 1)
     tools = mongo.db.tools.find().sort("tools", 1)
+    ingredients = mongo.db.ingredients.find().sort("ingredients", 1)
+    preparation = mongo.db.ingredients.find().sort("preparation", 1)
+    created_by = mongo.db.created_by.find("created_by")
     return render_template(
         "add_recipe.html", categories=categories, 
         servings=servings, leftovers=leftovers, 
-        prep_times=prep_times, cook_times=cook_times, tools=tools)
+        prep_times=prep_times, cook_times=cook_times, 
+        tools=tools, ingredients=ingredients, 
+        preparation=preparation, created_by=created_by)
 
 
 @app.route("/about")
@@ -146,7 +151,35 @@ def logout():
 
 @app.route("/single_recipe")
 def single_recipe():
-    return render_template("single_recipe.html", page_title="Vegetarian Chili")
+    if request.method == "POST":
+        recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "description": request.form.get("description"),
+            "category_name": request.form.get("category_name"),
+            "cost": request.form.get("cost"),
+            "serving_size": request.form.get("serving_size"),
+            "leftover_days": request.form.get("leftover_days"),
+            "prep_time": request.form.get("prep_time"),
+            "cook_time": request.form.get("cook_time"),
+            "tools": request.form.getlist("tools"),
+            "ingredients": request.form.getlist("ingredients"),
+            "preparation": request.form.getlist("preparation"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.insert_one(recipe)
+        flash("Task Successfully Added")
+        return redirect(url_for("single_recipe"))
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    servings = mongo.db.servings.find().sort("serving_size", 1)
+    leftovers = mongo.db.leftovers.find().sort("leftover_days", 1)
+    prep_times = mongo.db.prep_times.find().sort("prep_time", 1)
+    cook_times = mongo.db.cook_times.find().sort("cook_time", 1)
+    tools = mongo.db.tools.find().sort("tools", 1)
+    return render_template(
+        "single_recipe.html", categories=categories, 
+        servings=servings, leftovers=leftovers, 
+        prep_times=prep_times, cook_times=cook_times, tools=tools)
+
 
 
 if __name__ == "__main__":
