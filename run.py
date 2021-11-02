3d1895fabadcc4f145bd0ffbc695ace6cec96762
kashinak <kashinakfx@gmail.com>
2021-11-02 21:04:40 +0000

    Add email key to 'register' dictionary within register function in run.py

--- run.py
+++ run.py
@@ -92,6 +92,7 @@ def register():
         # dictionary 'register'
         register = {
             "username": request.form.get("username").lower(),
+            "email": request.form.get("email").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
