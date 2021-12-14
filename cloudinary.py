e9fb76b90179a378e6d7fb87e93d5b82ce1614c5
kashinak <kashinakfx@gmail.com>
2021-12-14 05:01:57 +0000

    Delete your_recipes.html template

--- cloudinary.py
+++
@@ -1,6 +0,0 @@
-cloudinary.api.create_upload_preset(
-  name = "dcll7ella",
-  unsigned = True, 
-  categorization = "google_tagging,google_video_tagging",
-  auto_tagging = 0.75,
-  folder = "kitchen_genie")
